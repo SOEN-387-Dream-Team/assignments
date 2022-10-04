cc6e76c6c52b5d823203f29dd04fa0c70a46e3f7
Semi-Demi <91032290+Semi-Demi@users.noreply.github.com>
2022-10-04 14:49:00 -0400

    Add files via upload

--- a1/php/courseReport.php
+++ a1/php/courseReport.php
@@ -1,4 +1,28 @@
-<?php
+<?php   
 
-//TODO: Given a course code, display all students taking that specific course
+    require_once('conn.php');
+    if (isset($_POST['course_report_button'])) // TODO: Change with respective html form name
+    { 
+        $course = $_POST['courseID'];
+
+        $sql = "SELECT id FROM student_courses WHERE courseCode=$course";
+
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0)
+        {
+            echo "Students enrolled in this course:";
+
+            while($row= $result->fetch_assoc())
+            {
+                echo $row['id'];
+            }
+        }
+        else
+        {
+            echo "No records found.";
+        }
+    }
+    
+
 ?>
--- a1/php/studentReport.php
+++ a1/php/studentReport.php
@@ -1,4 +1,28 @@
-<?php
+<?php   
 
-//TODO: Given a student name, output all the courses the student is registered for
+    require_once('conn.php');
+    if (isset($_POST['student_report_button'])) // TODO: Change with respective html form name
+    { 
+        $student = $_POST['studentID'];
+
+        $sql = "SELECT courseCode FROM student_courses WHERE id=$student";
+
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0)
+        {
+            echo " Courses you are registered in:";
+
+            while($row= $result->fetch_assoc())
+            {
+                echo $row['courseCode'];
+            }
+        }
+        else
+        {
+            echo "No records found.";
+        }
+    }
+    
+
 ?>
