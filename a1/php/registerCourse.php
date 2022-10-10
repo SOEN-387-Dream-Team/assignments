d8b79485330d9ed0d2a28d29689a45b223a873c1
Semi-Demi <91032290+Semi-Demi@users.noreply.github.com>
2022-10-10 03:38:57 -0400

    Add files via upload

--- a1/php/registerCourse.php
+++ a1/php/registerCourse.php
@@ -7,22 +7,34 @@
         $course = $_POST['addCode'];
         $currentDate = date('yyyyddMM');
 
-
-        $dateSql = "SELECT $course.startDate, $course.endDate 
-                    FROM courses
-                    SELECT startDate, endDate FROM courses c WHERE (c.startDate + 7) < $currentDate AND (c.endDate + 7) > currentDate AND c.courseCode =?";
-
-        $stmt = $conn->prepare($dateSql);
-        $stmt->bind_param("s", $course);
+        $numEnroll = "SELECT COUNT(courseCode) FROM student_courses s WHERE s.id =?";
+        $stmt = $conn->prepare($numEnroll);
+        $stmt->bind_param("s", $student);
         $stmt->execute();
         $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
 
-        if($result->num_rows > 0)
+        if($result > 5)
         {
-            $sql = "INSERT INTO student_courses (id, courseCode) VALUES($student, $course)";
+            echo $stmt->error;
+            echo "Cannot enroll in more than 5 courses at a time";
+        }
+        else
+        {
+            
+            $dateSql = "SELECT $course.startDate, $course.endDate 
+            FROM courses
+            SELECT startDate, endDate FROM courses c WHERE (c.startDate + 7) < $currentDate AND (c.endDate + 7) > currentDate AND c.courseCode =?";
 
-            $stmt = $conn->prepare($sql);
+            $stmt = $conn->prepare($dateSql);
+            $stmt->bind_param("s", $course);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if($result->num_rows > 0)
+            {
+                $sql = "INSERT INTO student_courses (id, courseCode) VALUES($student, $course)";
+
+                $stmt = $conn->prepare($sql);
 
             if ($stmt->execute()) 
             {
@@ -33,13 +45,14 @@
                 echo $stmt->error;
                 echo "Something went wrong. Course was not registered";
             }
-        }
-        else
-        {
-            echo "Requirements for course registration not met.";
+            }
+            else
+            {
+                echo "Requirements for course registration not met.";
+            }
+
         }
 
-        
         header("Refresh:8; url=../html/StudentPage.html");
     }
 
