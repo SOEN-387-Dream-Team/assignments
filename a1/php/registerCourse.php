5adc914fffc820b784db0828187f35d461a5d47d
Mauran Pavan <mauranpavan@gmail.com>
2022-10-10 18:53:35 -0400

    fixed the sql aspect of the registerCourse(aka addCourse) issue, session id is set to 6 atm

--- a1/php/registerCourse.php
+++ a1/php/registerCourse.php
@@ -3,27 +3,30 @@
     require_once('conn.php');
     if (isset($_POST['addCourse']))
     { 
-        $student = $_SESSION['id'];
+        $student = 6;// $_SESSION['id'];
         $course = $_POST['addCode'];
-        $currentDate = date('yyyyddMM');
+        $currentDate = date('Y-m-d');
 
         $numEnroll = "SELECT COUNT(courseCode) FROM student_courses s WHERE s.id =?";
         $stmt = $conn->prepare($numEnroll);
-        $stmt->bind_param("s", $student);
+        $stmt->bind_param("i", $student);
         $stmt->execute();
         $result = $stmt->get_result();
+        
 
-        if($result > 5)
+        if($result->num_rows > 5)
         {
             echo $stmt->error;
             echo "Cannot enroll in more than 5 courses at a time";
         }
         else
         {
-            
-            $dateSql = "SELECT $course.startDate, $course.endDate 
-            FROM courses
-            SELECT startDate, endDate FROM courses c WHERE (c.startDate + 7) < $currentDate AND (c.endDate + 7) > currentDate AND c.courseCode =?";
+            //Purpose of this sql is to check if we are within the 1 week time limit to add the course
+            $dateSql = "SELECT startDate 
+                        FROM courses c 
+                        WHERE CURDATE() > c.startDate 
+                        AND CURDATE() < DATE_ADD(c.startDate, INTERVAL 7 DAY) 
+                        AND c.courseCode = UPPER(?)";
 
             $stmt = $conn->prepare($dateSql);
             $stmt->bind_param("s", $course);
@@ -32,23 +35,24 @@
 
             if($result->num_rows > 0)
             {
-                $sql = "INSERT INTO student_courses (id, courseCode) VALUES($student, $course)";
+                $sql = "INSERT INTO student_courses(id, courseCode) VALUES(?, UPPER(?))";
 
                 $stmt = $conn->prepare($sql);
+                $stmt->bind_param("is", $student, $course);
 
-            if ($stmt->execute()) 
-            {
-                echo "Registered Successfully!";
-            } 
-            else 
-            {
-                echo $stmt->error;
-                echo "Something went wrong. Course was not registered";
-            }
+                if ($stmt->execute()) 
+                {
+                    echo "Registered Successfully!";
+                } 
+                else 
+                {
+                    echo $stmt->error;
+                    echo "Something went wrong. Course was not registered";
+                }
             }
             else
             {
-                echo "Requirements for course registration not met.";
+                echo "Requirements for course registration not met: You have passed the deadline to enroll in this course.";
             }
 
         }
