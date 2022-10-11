ec4c6b365dfc77ae0ff18892648b275d83e2e1af
Gabriel Martinica <gabmartinica@gmail.com>
2022-10-11 00:17:42 -0400

    Add student courses add/drop as dropdown

---
+++ a1/php/nonEnrolledCourseStudent.php
@@ -0,0 +1,20 @@
+<?php
+require_once("conn.php");
+session_start();
+
+
+$studentSql ="SELECT * FROM courses WHERE courseCode NOT IN (SELECT courseCode from student_courses WHERE id=?)";
+
+$stmt = $conn->prepare($studentSql);
+$stmt->bind_param("i", $_SESSION['id']);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result) {
+  while( $row = $result->fetch_assoc() ) {
+    echo "<option value='".$row['courseCode']."'>".$row['courseCode']."</option>";
+  }
+  $result->free();
+}
+
+?>
